b0b4eef6a04ee30bd04137e8cdb871dd37bbbbf5
hyerim <chr1105@naver.com>
2025-08-21 00:51:08 +0900

    fix : 임시로 전체 경로 허용

--- src/main/java/com/example/tiggle/config/SecurityConfig.java
+++ src/main/java/com/example/tiggle/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/api/**").permitAll()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()
             );
         return http.build();
