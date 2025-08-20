01ff455f7dfc58cd450a7d2c448c05a1624df256
hyerim <chr1105@naver.com>
2025-08-20 18:32:57 +0900

    fix: 이메일 인증 코드 보완 - 스웨거 문서 설명 추가 - 코드 주석 추가 - Department, University 엔티티 추가

---
+++ src/main/java/com/example/tiggle/entity/University.java
@@ -0,0 +1,15 @@
+package com.example.tiggle.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
