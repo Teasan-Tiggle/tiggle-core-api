e4055addcb2ef77574bf7aaf259df7901faab152
hyerim <chr1105@naver.com>
2025-08-20 18:32:57 +0900

    fix: 이메일 인증 코드 보완 - 스웨거 문서 설명 추가 - 코드 주석 추가 - Department, University 엔티티 추가

--- src/main/java/com/example/tiggle/controller/auth/EmailAuthController.java
+++ src/main/java/com/example/tiggle/controller/auth/EmailAuthController.java
@@ -6,6 +6,7 @@ import com.example.tiggle.dto.auth.EmailVerifyRequestDto;
 import com.example.tiggle.service.auth.EmailAuthService;
 import com.example.tiggle.service.user.StudentService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -33,36 +34,47 @@ public class EmailAuthController {
      * @return 전송 결과
      */
     @PostMapping("/send")
-    @Operation(summary = "이메일 인증 번호 전송", description = "입력한 이메일 주소로 인증코드를 전송합니다.")
+    @Operation(summary = "이메일 인증코드 전송", description = "입력한 이메일 주소로 인증코드를 전송합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "인증 번호 전송 결과 반환"),
             @ApiResponse(responseCode = "400", description = "요청 형식 오류 (이메일 누락)", content = @Content)
     })
-    public ResponseEntity<?> sendEmail(@Valid @RequestBody EmailSendRequestDto requestDto) {
+    public ResponseEntity<?> sendEmail(
+            @Parameter(description = "이메일 정보 (JSON 형식)", required = true)
+            @Valid @RequestBody EmailSendRequestDto requestDto
+    ) {
 
         if (requestDto.getEmail() == null)
             return ResponseEntity.badRequest().build();
 
         if (studentService.checkDuplicateEmail(requestDto.getEmail()))
-            return ResponseEntity.ok(new ResponseDto(false, "이미 가입된 이메일입니다."));
+            return ResponseEntity.ok(new ResponseDto<Void>(false, "이미 가입된 이메일입니다."));
 
         emailAuthService.sendVerificationCode(requestDto.getEmail());
 
-        return ResponseEntity.ok(new ResponseDto(true));
+        return ResponseEntity.ok(new ResponseDto<Void>(true));
     }
 
+    /**
+     * 입력한 이메일과 인증코드로 검증한다.
+     *
+     * @return 검증 결과
+     */
     @PostMapping("/verify")
-    @Operation(summary = "이메일 인증 번호 검증", description = "입력한 이메일과 인증코드를 검증합니다.")
+    @Operation(summary = "이메일 인증코드 검증", description = "입력한 이메일과 인증코드를 검증합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "이메일 인증 결과 반환"),
             @ApiResponse(responseCode = "400", description = "요청 형식 오류 (이메일 또는 코드 누락)", content = @Content)
     })
-    public ResponseEntity<ResponseDto> verifyCode(@Valid @RequestBody EmailVerifyRequestDto requestDto) {
+    public ResponseEntity<ResponseDto<Void>> verifyCode(
+            @Parameter(description = "이메일 인증코드 정보 (JSON 형식)", required = true)
+            @Valid @RequestBody EmailVerifyRequestDto requestDto
+    ) {
         boolean isVerified = emailAuthService.verifyCode(requestDto.getEmail(), requestDto.getCode());
         if (isVerified) {
-            return ResponseEntity.ok(new ResponseDto(true));
+            return ResponseEntity.ok(new ResponseDto<Void>(true));
         } else {
-            return ResponseEntity.badRequest().body(new ResponseDto(false, "인증 코드가 유효하지 않습니다."));
+            return ResponseEntity.badRequest().body(new ResponseDto<Void>(false, "인증 코드가 유효하지 않습니다."));
         }
     }
 }
