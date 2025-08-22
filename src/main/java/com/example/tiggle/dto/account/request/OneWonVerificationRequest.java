d8b528f25535ec8e235bb25129359ca4148b138b
이종현 <jhyeon2450@gmail.com>
2025-08-22 22:40:49 +0900

    feat: OPS-55 1원 송금 요청 DTO 구현

---
+++ src/main/java/com/example/tiggle/dto/account/request/OneWonVerificationRequest.java
@@ -0,0 +1,14 @@
+package com.example.tiggle.dto.account.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Schema(description = "1원 송금 요청 DTO")
+public class OneWonVerificationRequest {
+    
+    @Schema(description = "계좌번호", example = "0888367052216382", required = true)
+    private String accountNo;
+}
