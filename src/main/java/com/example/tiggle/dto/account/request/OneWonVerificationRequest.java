49ae2edd28c9ead04687969f2a3947ebeddc7ec3
이종현 <jhyeon2450@gmail.com>
2025-08-25 23:22:31 +0900

    feat: account 관련 예외 처리 추가

--- src/main/java/com/example/tiggle/dto/account/request/OneWonVerificationRequest.java
+++ src/main/java/com/example/tiggle/dto/account/request/OneWonVerificationRequest.java
@@ -1,6 +1,7 @@
 package com.example.tiggle.dto.account.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Schema(description = "1원 송금 요청 DTO")
 public class OneWonVerificationRequest {
-    
+
+    @NotNull
     @Schema(description = "계좌번호", example = "0888367052216382", required = true)
     private String accountNo;
 }
--- src/main/java/com/example/tiggle/dto/account/request/PrimaryAccountRequest.java
+++ src/main/java/com/example/tiggle/dto/account/request/PrimaryAccountRequest.java
@@ -1,6 +1,7 @@
 package com.example.tiggle.dto.account.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,9 +10,11 @@ import lombok.NoArgsConstructor;
 @Schema(description = "주 계좌 등록 요청 DTO")
 public class PrimaryAccountRequest {
     
+    @NotNull
     @Schema(description = "계좌번호", example = "0888367052216382", required = true)
     private String accountNo;
     
+    @NotNull
     @Schema(description = "계좌 검증 토큰", example = "abc123-def456-ghi789", required = true)
     private String verificationToken;
 }
