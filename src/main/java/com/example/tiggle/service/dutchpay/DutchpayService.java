9d6d9f6140bfce45718ca58e2ccc8abf3cd163c4
heojimyeong <wlaud0823@naver.com>
2025-08-28 19:31:58 +0900

    feat : OPS-43 더치페이 내역 조회 수정 및 상세 조회 수정

--- src/main/java/com/example/tiggle/service/dutchpay/DutchpayService.java
+++ src/main/java/com/example/tiggle/service/dutchpay/DutchpayService.java
@@ -2,12 +2,13 @@ package com.example.tiggle.service.dutchpay;
 
 import com.example.tiggle.dto.dutchpay.request.CreateDutchpayRequest;
 import com.example.tiggle.dto.dutchpay.request.DutchpayDetailData;
+import com.example.tiggle.dto.dutchpay.response.DutchpayDetailResponse;
 import com.example.tiggle.dto.dutchpay.response.DutchpayListResponse;
 import com.example.tiggle.dto.dutchpay.response.DutchpaySummaryResponse;
 
 public interface DutchpayService {
     void create(String encryptedUserKey, Long creatorId, CreateDutchpayRequest req);
-    DutchpayDetailData getDetail(Long dutchpayId, Long userId);
+    DutchpayDetailResponse getDetail(Long dutchpayId, Long userId);
     DutchpayListResponse getDutchpayListCursor(Long userId, String tab, String cursor, Integer size);
     DutchpaySummaryResponse getSummary(String encryptedUserKey, Long userId);
 }
