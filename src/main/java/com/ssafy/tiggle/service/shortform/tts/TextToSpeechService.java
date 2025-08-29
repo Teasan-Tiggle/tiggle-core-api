2eaf3c32a07e3a9560fdb4fb9c2a8db522315140
2-jjong <jhyeon2450@gmail.com>
2025-08-30 02:57:41 +0900

    refactor: Veo3 영상 생성에 음성 추가, TTS API 삭제

--- src/main/java/com/ssafy/tiggle/service/shortform/tts/TextToSpeechService.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tiggle.service.shortform.tts;
-
-import java.util.List;
-
-public interface TextToSpeechService {
-
-    List<byte[]> synthesizeSpeechKoreanBySentences(String script);
-}
